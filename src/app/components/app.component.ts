72fd48944ceb0c1103a3ebe270d4dc27cde521a6
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-04-09 22:54:05 +0300

    s

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>das</h1>'
+  templateUrl: '../templates/app.html'
 })
 export class AppComponent {
   title = 'app works!';
+  name:string="Tom";
+  age:number = 24;
+  count: number=0;
+  increase() : void {
+      this.count++;
+  }
 }
--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from '../components/app.component';
+import { ChildComponent } from '../components/child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
