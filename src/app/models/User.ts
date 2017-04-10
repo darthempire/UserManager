31ed8157c84332dce600d54306ed4f39678b9fce
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-04-10 23:26:23 +0300

    nothing

---
+++ src/app/models/User.ts
@@ -0,0 +1,13 @@
+export class People {
+  private Id: number;
+  private RoleName: string;
+  private Email: string;
+  private EmailConfirmed: boolean;
+  private PhoneNumber: string;
+  private DateRegistration: string;
+
+  constructor(){
+
+  }
+}
+
