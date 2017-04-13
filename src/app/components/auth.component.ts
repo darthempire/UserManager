532ca6dc0cca13c23dac7a42840de0eac51a1bff
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-04-13 19:25:19 +0300

    dependency injection

--- src/app/components/auth.component.ts
+++ src/app/components/auth.component.ts
@@ -1,26 +1,36 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { LogService } from '../services/log.service';
+import { UserService } from '../services/user.service';
+
+import { User } from '../models/User';
 
 @Component({
     selector: 'auth-comp',
     templateUrl: '../templates/auth/index.html',
-	styleUrls: ['../templates/auth/css/master.css']
+    styleUrls: ['../templates/auth/css/master.css'],
+    providers: [ UserService, LogService ]
 })
 
 export class AuthComponent {
     title: string;
     name: string = "Denchik";
     passwordHash: string;
+    items: User[] = [];
 
-	@Input() userName: string;
+    constructor(private userService: UserService) {
+        console.log(userService.getPeoples());
+    }
 
-	@Output() userNameChange  = new EventEmitter<string>();
-	onNameChange(model: string) {
-		this.userName = model;
-		this.userNameChange.emit(model);
-	}
+    @Input() userName: string;
 
-	@Output() onChanged = new EventEmitter<boolean>();
-	change(increased) {
-		this.onChanged.emit(increased);
-	}
+    @Output() userNameChange = new EventEmitter<string>();
+    onNameChange(model: string) {
+        this.userName = model;
+        this.userNameChange.emit(model);
+    }
+
+    @Output() onChanged = new EventEmitter<boolean>();
+    change(increased) {
+        this.onChanged.emit(increased);
+    }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,32 +1,40 @@
+import { Injectable } from '@angular/core';
 import { User } from '../models/User';
+import { LogService } from '../services/log.service';
 
+@Injectable()
 export class UserService {
-  
-  private data: User[] = [
-    new User("Petya", 12),
-    new User("Natasha", -12)
-  ];
 
-  getPeoples(): User[] {
-    return this.data;
-  }
+    private data: User[] = [
+        new User("Petya", 12),
+        new User("Natasha", -12)
+    ];
 
-  addPeople(name: string, mark: number) {
-    if(name === "" || mark === undefined)
-      console.log("fsda");
-    else
-      this.data.push(new User(name, mark));
+	constructor(private logService: LogService) {
 
-  }
+	}
 
-  delPeople(guid: string) {
-    this.data.splice(this.find(guid), 1);
-  }
+    getPeoples(): User[] {
+		this.logService.write("операция получения данных");
+        return this.data;
+    }
 
-  private find(guid: string): number {
-      for (let i: number = 0; i < this.data.length; i++) {
-        if (this.data[i].guid == guid) return i;
-      }
-  }
+    addPeople(name: string, mark: number) {
+        if (name === "" || mark === undefined)
+            console.log("fsda");
+        else
+            this.data.push(new User(name, mark));
+
+    }
+
+    delPeople(guid: string) {
+        this.data.splice(this.find(guid), 1);
+    }
+
+    private find(guid: string): number {
+        for (let i: number = 0; i < this.data.length; i++) {
+            if (this.data[i].guid == guid) return i;
+        }
+    }
 
 }
