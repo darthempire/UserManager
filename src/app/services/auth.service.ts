229e714e45cfd6326064fc0147917bd3b1ac9366
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-04-17 00:32:16 +0300

    work with authentification

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -13,9 +13,11 @@ import { User, UserRegistrationModel, UserLoginModel } from '../models/User';
 export class AuthService {
 
     user: User;
-    authentification: any = {
+
+    public authentification: any = {
         isAuth: true,
-        userName: ""
+        userName: "",
+        refreshToken: ""
     };
 
     constructor(private httpService: HttpService) { }
@@ -42,9 +44,53 @@ export class AuthService {
                             userName: data.userName,
                             refreshToken: data.refresh_token
                         }
-					)
+                    )
                 );
+                this.authentification.isAuth = true;
+                this.authentification.userName = data.userName;
             });
+    }
+
+    RefreshToken() {
+		let authData = JSON.parse(localStorage.getItem('authorizationData'));
+        let body = "grant_type=password&username=" + authData.UserName
+            + "&password=" + authData.Password
+            + "&client_id=" + Global.clientId;
+
+        let response = this.httpService.loginPost(Global.apiServiceBaseUriL + 'token', body)
+            .map((resp: Response) => {
+                return resp.json();
+            }).subscribe((data) => {
+                localStorage.setItem(
+                    'authorizationData',
+                    JSON.stringify(
+                        {
+                            token: data.access_token,
+                            userName: data.userName,
+                            refreshToken: data.refresh_token
+                        }
+                    )
+                );
+                this.authentification.isAuth = true;
+                this.authentification.userName = data.userName;
+            });
+    }
+
+    LogOut() {
+        localStorage.removeItem("authorizationData");
+
+        this.authentification.isAuth = false;
+        this.authentification.userName = "";
+        this.authentification.refreshToken = "";
+    }
+
+    FillAuthData() {
+        let authData = JSON.parse(localStorage.getItem("authorizationData"));
+        if (authData) {
+            this.authentification.isAuth = true;
+            this.authentification.userName = authData.userName;
+            this.authentification.refreshToken = authData.refreshToken;
+        }
     }
 
     getSomething() {
