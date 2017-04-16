7f4731b7201dc3d082d2a5f5d929e16c7ac0edce
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-04-16 15:24:44 +0300

    override http

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '../helpers/HttpClient';
+import { Injectable } from '@angular/core';
 
 export class Item {
 
@@ -11,25 +13,33 @@ export class Item {
 @Component({
     selector: 'app-root',
     templateUrl: '../templates/app/app.html',
-    styleUrls: ['../templates/app/css/app.css']
+    styleUrls: ['../templates/app/css/app.css'],
+    providers: [HttpClient]
 })
 
+@Injectable()
 export class AppComponent {
 
     item: Item = new Item();
 
-    constructor(private router: Router) { }
+    constructor(private router: Router, private http: HttpClient) { }
+
+    sendSomething() {
+        this.http.get("http://localhost:64183/api/values").subscribe(result => {
+            console.log( result );
+        });
+    }
 
     goToItem(myItem: Item) {
 
         this.router.navigate(
             ['/item', myItem.id],
-			{
-				queryParams: {
-					'product': myItem.product,
-					'price': myItem.price
-				}
-			}
+            {
+                queryParams: {
+                    'product': myItem.product,
+                    'price': myItem.price
+                }
+            }
         );
     }
 }
