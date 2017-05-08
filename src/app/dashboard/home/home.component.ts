e90cdea588519152f53ce39a6901da20a7ebe9f7
vasyamakarchuk <vasyamakarchuk@outlook.com>
2017-05-08 16:22:54 +0300

    last

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
-//import initDemo = require('../../../assets/js/charts.js');
+// import initDemo = require('../../../assets/js/charts.js');
 
 declare var $:any;
 
@@ -11,12 +11,14 @@ declare var $:any;
 
 export class HomeComponent implements OnInit{
     ngOnInit(){
+        // var initDemo = $.getScript('./../../assets/js/charts.js');
         // $('[data-toggle="checkbox"]').each(function () {
         //     if($(this).data('toggle') == 'switch') return;
         //
         //     var $checkbox = $(this);
         //     $checkbox.checkbox();
         // });
-//        initDemo();
+        // initDemo();
+
     }
 }
