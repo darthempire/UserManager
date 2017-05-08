876f7f66a7bf23ae9b869451eb42f15d1101f7d6
vasyamakarchuk <vasyamakarchuk@outlook.com>
2017-05-08 14:10:39 +0300

    some new components

--- src/app/dashboard/dashboard.routes.ts
+++ src/app/dashboard/dashboard.routes.ts
@@ -4,15 +4,18 @@ import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { IconsComponent } from './icons/icons.component';
 import { TableComponent } from './table/table.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { TypographyComponent } from './typography/typography.component';
+import { MapsComponent } from './maps/maps.component';
 
 export const MODULE_ROUTES: Route[] =[
     { path: 'dashboard', component: HomeComponent },
     { path: 'user', component: UserComponent },
     { path: 'table', component: TableComponent },
     { path: 'icons', component: IconsComponent },
-    // { path: 'notifications', component: NotificationsComponent },
-    // { path: 'typography', component: TypographyComponent },
-    // { path: 'maps', component: MapsComponent },
+    { path: 'notifications', component: NotificationsComponent },
+    { path: 'typography', component: TypographyComponent },
+    { path: 'maps', component: MapsComponent },
     // { path: 'upgrade', component: UpgradeComponent },
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
 ]
@@ -21,5 +24,9 @@ export const MODULE_COMPONENTS = [
     HomeComponent,
     UserComponent,
     TableComponent,
-    IconsComponent
+    IconsComponent,
+    NotificationsComponent,
+    TypographyComponent,
+    MapsComponent
+    // UpgradeComponent
 ]
