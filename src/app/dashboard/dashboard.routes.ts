70575dc8b2ffb1bfd53fadd28d72b3afb5fbcf9a
vasyamakarchuk <vasyamakarchuk@outlook.com>
2017-05-08 14:01:29 +0300

    add some components

--- src/app/dashboard/dashboard.routes.ts
+++ src/app/dashboard/dashboard.routes.ts
@@ -1,12 +1,15 @@
 import { Route } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { IconsComponent } from './icons/icons.component';
+import { TableComponent } from './table/table.component';
 
 export const MODULE_ROUTES: Route[] =[
     { path: 'dashboard', component: HomeComponent },
-    // { path: 'user', component: UserComponent },
-    // { path: 'table', component: TableComponent },
-    // { path: 'icons', component: IconsComponent },
+    { path: 'user', component: UserComponent },
+    { path: 'table', component: TableComponent },
+    { path: 'icons', component: IconsComponent },
     // { path: 'notifications', component: NotificationsComponent },
     // { path: 'typography', component: TypographyComponent },
     // { path: 'maps', component: MapsComponent },
@@ -16,4 +19,7 @@ export const MODULE_ROUTES: Route[] =[
 
 export const MODULE_COMPONENTS = [
     HomeComponent,
+    UserComponent,
+    TableComponent,
+    IconsComponent
 ]
